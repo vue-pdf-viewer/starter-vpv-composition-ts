832ed7b1bf3e5139510858ad2b02c7540bde7752
haroonth <kittisak.ma@logicspark.com>
2024-07-03 16:37:41 +0700

    💄 Update example: add mobile view

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,8 @@
 
 <template>
   <div>
-    <h1>VPV Starter Toolkit</h1>
+    <h1>VPV Starter Toolkit: Vue + Composition API + TypeScript</h1>
+    <br />
     <h2>Default Toolbar</h2>
     <div class="pdf-viewer-wrapper">
       <VPdfViewer
@@ -16,6 +17,11 @@
       <VPdfViewer src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
         :toolbar-options="toolbarOptions" />
     </div>
+    <h2>Mobile</h2>
+    <div class="pdf-viewer-wrapper-mobile">
+      <VPdfViewer
+        src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+    </div>
   </div>
 </template>
 
@@ -23,5 +29,12 @@
   .pdf-viewer-wrapper {
     width: 1028px;
     height: 700px;
+    margin: 0 auto;
+  }
+
+  .pdf-viewer-wrapper-mobile {
+    width: 768px;
+    height: 700px;
+    margin: 0 auto;
   }
 </style>
