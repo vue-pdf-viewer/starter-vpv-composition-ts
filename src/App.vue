e838174107bb3aacce2d12f4a7c6f9b8d6dcd254
haroonth <kittisak.ma@logicspark.com>
2024-07-03 16:07:51 +0700

    ✨ utilize VPV in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { VPdfViewer, type ToolbarOptions } from '@vpv/viewer';
+  const toolbarOptions: Partial<ToolbarOptions> | false = false
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>VPV Starter Toolkit</h1>
+    <h2>Default Toolbar</h2>
+    <div class="pdf-viewer-wrapper">
+      <VPdfViewer
+        src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+    </div>
+    <h2>Without Toolbar</h2>
+    <div class="pdf-viewer-wrapper">
+      <VPdfViewer src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
+        :toolbar-options="toolbarOptions" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .pdf-viewer-wrapper {
+    width: 1028px;
+    height: 700px;
+  }
 </style>
