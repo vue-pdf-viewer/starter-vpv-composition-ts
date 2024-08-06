b06d2e54f7b89df32ad105f27bdeb06d239aa5b5
haroonth <kittisak.ma@logicspark.com>
2024-08-06 19:40:51 +0700

    📝 update example of using VPV component globally

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
   import { VPdfViewer, type ToolbarOptions, useLicense } from '@vue-pdf-viewer/viewer';
+  import AppPdfViewer from './components/AppPdfViewer.vue';
   const toolbarOptions: Partial<ToolbarOptions> | false = false
   useLicense({ licenseKey: 'your-license-key' })
+
+  const pdfSrc ="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
 </script>
 
 <template>
@@ -10,19 +13,18 @@
     <br />
     <h2>Default Toolbar</h2>
     <div class="pdf-viewer-wrapper">
-      <VPdfViewer
-        src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+      <VPdfViewer :src="pdfSrc" />
     </div>
     <h2>Without Toolbar</h2>
     <div class="pdf-viewer-wrapper">
-      <VPdfViewer src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
+      <VPdfViewer :src="pdfSrc"
         :toolbar-options="toolbarOptions" />
     </div>
     <h2>Mobile</h2>
     <div class="pdf-viewer-wrapper-mobile">
-      <VPdfViewer
-        src="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf" />
+      <VPdfViewer :src="pdfSrc" />
     </div>
+    <AppPdfViewer :src="pdfSrc" title="Shared Component" />
   </div>
 </template>
 
---
+++ src/components/AppPdfViewer.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { VPdfViewer, VPVBaseProps } from '@vue-pdf-viewer/viewer';
+
+  const props = defineProps({
+      ...VPVBaseProps, 
+      title: {
+        type: String,
+        required: true
+      }
+    } as const
+  )
+</script>
+<template>
+  <div>
+    <h2>
+      {{ props.title }}
+    </h2>
+    <div :style="{ width: '1028px', height: '700px', margin: '0 auto'}">
+      <VPdfViewer v-bind="props" />
+    </div>
+  </div>
+</template>
