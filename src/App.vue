0383c8710e75e1bcfb7ecec29b6a17b17a19ac43
haroonth <kittisak.ma@logicspark.com>
2024-11-08 18:27:28 +0700

    📝 update shared VPV component

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,46 @@
 <script setup lang="ts">
-  import { VPdfViewer, type ToolbarOptions, useLicense } from '@vue-pdf-viewer/viewer';
-  import AppPdfViewer from './components/AppPdfViewer.vue';
-  const toolbarOptions: Partial<ToolbarOptions> | false = false
-  useLicense({ licenseKey: 'your-license-key' })
+	import { VPdfViewer, type ToolbarOptions } from "@vue-pdf-viewer/viewer";
+	import AppPdfViewer from "./components/AppPdfViewer.vue";
+	const toolbarOptions: Partial<ToolbarOptions> | false = false;
 
-  const pdfSrc ="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf"
+	const pdfSrc =
+		"https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
 </script>
 
 <template>
-  <div>
-    <h1>VPV Starter Toolkit: Vue + Composition API + TypeScript</h1>
-    <br />
-    <h2>Default Toolbar</h2>
-    <div class="pdf-viewer-wrapper">
-      <VPdfViewer :src="pdfSrc" />
-    </div>
-    <h2>Without Toolbar</h2>
-    <div class="pdf-viewer-wrapper">
-      <VPdfViewer :src="pdfSrc"
-        :toolbar-options="toolbarOptions" />
-    </div>
-    <h2>Mobile</h2>
-    <div class="pdf-viewer-wrapper-mobile">
-      <VPdfViewer :src="pdfSrc" />
-    </div>
-    <AppPdfViewer :src="pdfSrc" title="Shared Component" />
-  </div>
+	<div>
+		<h1>VPV Starter Toolkit: Vue + Composition API + TypeScript</h1>
+		<br />
+		<h2>Default Toolbar</h2>
+		<div class="pdf-viewer-wrapper">
+			<VPdfViewer :src="pdfSrc" />
+		</div>
+		<h2>Without Toolbar</h2>
+		<div class="pdf-viewer-wrapper">
+			<VPdfViewer
+				:src="pdfSrc"
+				:toolbar-options="toolbarOptions" />
+		</div>
+		<h2>Mobile</h2>
+		<div class="pdf-viewer-wrapper-mobile">
+			<VPdfViewer :src="pdfSrc" />
+		</div>
+		<AppPdfViewer
+			:src="pdfSrc"
+			title="Shared Component" />
+	</div>
 </template>
 
 <style scoped>
-  .pdf-viewer-wrapper {
-    width: 1028px;
-    height: 700px;
-    margin: 0 auto;
-  }
+	.pdf-viewer-wrapper {
+		width: 1028px;
+		height: 700px;
+		margin: 0 auto;
+	}
 
-  .pdf-viewer-wrapper-mobile {
-    width: 468px;
-    height: 700px;
-    margin: 0 auto;
-  }
+	.pdf-viewer-wrapper-mobile {
+		width: 468px;
+		height: 700px;
+		margin: 0 auto;
+	}
 </style>
--- src/components/AppPdfViewer.vue
+++ src/components/AppPdfViewer.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
-  import { VPdfViewer, VPVBaseProps } from '@vue-pdf-viewer/viewer';
+	import { VPdfViewer, VPVBaseProps, useLicense } from "@vue-pdf-viewer/viewer";
+	import { ref, watch, onBeforeMount } from "vue";
 
-  const props = defineProps({
-      ...VPVBaseProps, 
-      title: {
-        type: String,
-        required: true
-      }
-    } as const
-  )
+	const props = defineProps({
+		...VPVBaseProps,
+		title: {
+			type: String,
+			required: true,
+		},
+	} as const);
+
+	onBeforeMount(() => {
+		useLicense({ licenseKey: "your-license-key" });
+	});
+
+	const viewerRef = ref<InstanceType<typeof VPdfViewer> | null>(null);
+
+	watch(viewerRef, (newVal) => {
+		console.log("viewerRef", newVal);
+	});
 </script>
 <template>
-  <div>
-    <h2>
-      {{ props.title }}
-    </h2>
-    <div :style="{ width: '1028px', height: '700px', margin: '0 auto'}">
-      <VPdfViewer v-bind="props" />
-    </div>
-  </div>
+	<div>
+		<h2>
+			{{ props.title }}
+		</h2>
+		<div :style="{ width: '1028px', height: '700px', margin: '0 auto' }">
+			<VPdfViewer
+				v-bind="props"
+				ref="viewerRef" />
+		</div>
+	</div>
 </template>
